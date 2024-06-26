16577e512c3ca7682cf762963fc2d3dbf5781a42
Andre Ismael <andre.ismael@tjdft.jus.br>
2024-06-26 15:55:15 -0300

    Tentativa de correção

--- node/app.js
+++ node/app.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
     host: 'db',
     user: 'root',
     password: 'root',
-    database: 'nodedb'
+    database: 'nodedb',
+    port: '3306'
 });
 
 // Conectar ao MySQL
@@ -23,34 +24,34 @@ connection.connect((err) => {
 const app = express();
 const port = 3000;
 
-// Rota para fazer o SELECT no MySQL
+// Rota para fazer o SELECT no MySQL e inserir dados
 app.get('/', (req, res) => {
-    
-
     const nomeSorteado = sortearNome();
-    const pessoa = { name: nomeSorteado };
-    connection.query("INSERT INTO people (name) values('"+ nomeSorteado +"')",  (error, results) => {
+
+    // Inserir dados na tabela 'people' usando consulta parametrizada
+    connection.query('INSERT INTO people (name) VALUES (?)', [nomeSorteado], (error, results) => {
         if (error) {
             console.error('Erro ao inserir dados: ' + error.stack);
             return res.status(500).send('Erro ao inserir dados no banco de dados.');
         }
-    });
 
+        // Selecionar todos os registros da tabela 'people'
+        connection.query('SELECT * FROM people', (error, results, fields) => {
+            if (error) {
+                console.error('Erro ao consultar dados: ' + error.stack);
+                return res.status(500).send('Erro ao consultar dados no banco de dados.');
+            }
 
-    connection.query('SELECT * FROM people', (error, results, fields) => {
-        if (error) throw error;
+            // Construir resposta HTML com os resultados da consulta
+            let html = '<h1>Full Cycle Rocks!</h1><h2>Resultados da Consulta</h2><table border="1"><tr><th>ID</th><th>Nome</th></tr>';
+            results.forEach(result => {
+                html += `<tr><td>${result.id}</td><td>${result.name}</td></tr>`;
+            });
+            html += '</table>';
 
-
-        let html = '<h1>Full Cycle Rocks!</h1> <h1>Resultados da Consulta</h1><table border="1"><tr><th>ID</th><th>Nome</th></tr>';
-        results.forEach(result => {
-            html += `<tr><td>${result.id}</td><td>${result.name}</td></tr>`;
+            // Enviar resposta HTTP com a tabela HTML
+            res.send(html);
         });
-        html += '</table>';
-
-        // Exibe a tabela na resposta HTTP
-        res.send(html);
-
-
     });
 });
 
